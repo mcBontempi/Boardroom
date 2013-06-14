3f724c70888751b399ae77c9d763361f9cb3e01a
mcbontempi <mcbontempi@gmail.com>
2013-06-14 16:46:55 +0100

    just getting it all in

---
+++ Client/Boardroom/Boardroom/ViewController.m
@@ -0,0 +1,108 @@
+//
+//  ViewController.m
+//  Boardroom
+//
+//  Created by daren taylor on 09/05/2013.
+//  Copyright (c) 2013 daren taylor. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <AFNetworking/AFHTTPClient.h>
+#import <AFNetworking/AFHTTPRequestOperation.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@end
+
+@implementation ViewController 
+{
+  NSTimer *_timer;
+  AFHTTPClient *_client;
+}
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  
+  NSURLRequest *request = [[NSURLRequest alloc] initWithURL: [NSURL URLWithString: @"http://www.tfl.gov.uk/assets/downloads/standard-tube-map.pdf"] cachePolicy: NSURLRequestUseProtocolCachePolicy timeoutInterval: 100];
+  [self.webView loadRequest: request];
+  
+  self.webView.delegate = self;
+  
+  _client= [AFHTTPClient clientWithBaseURL:[NSURL URLWithString:@"http://162.13.5.127:8080"]];
+  
+  self.webView.scalesPageToFit=YES ;
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+  [self setupNewTimer];
+  
+  
+}
+
+- (void)setupNewTimer
+{
+  _timer = [NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(timerTick) userInfo:nil repeats:NO];
+}
+
+- (void)timerTick
+{
+  
+  
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+      [self sendScreenshot];
+    });
+  
+  
+  
+  
+}
+
+- (void)sendScreenshot
+{
+  
+
+  NSLog(@"start");
+  UIImage *image = [self captureScreen:[self webView]];
+  
+  NSLog(@"end");
+  
+  NSLog(@"height = %f", image.size.height);
+  
+  NSMutableURLRequest *request = [_client multipartFormRequestWithMethod:@"POST" path:@"upload" parameters:nil constructingBodyWithBlock: ^(id <AFMultipartFormData>formData) {
+    [formData appendPartWithFileData:UIImagePNGRepresentation(image) name:@"myFile" fileName:@"temp.png" mimeType:@"image/png"];
+  }];
+  
+  __weak id weakSelf = self;
+  AFHTTPRequestOperation *operation = [_client HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject){
+    NSString *response = [operation responseString];
+    NSLog(@"response: [%@]",response);
+    NSLog(@"complete");
+    [weakSelf setupNewTimer];
+    
+  } failure:^(AFHTTPRequestOperation *operation, NSError *error){
+    [weakSelf setupNewTimer];
+  }];
+
+  NSLog(@"send");
+  
+  [_client enqueueHTTPRequestOperation:operation];
+  
+}
+- (IBAction)send:(id)sender
+{
+  [self sendScreenshot];
+}
+-(UIImage*)captureScreen:(UIView*) viewToCapture{
+ UIGraphicsBeginImageContextWithOptions(viewToCapture.bounds.size,NO, 0.0);
+ // UIGraphicsBeginImageContext(viewToCapture.bounds.size);
+  
+  [viewToCapture.layer renderInContext:UIGraphicsGetCurrentContext()];
+  UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return viewImage;
+}
+
+@end
---
+++ Server/hello.py
@@ -0,0 +1,59 @@
+import cherrypy
+import os
+class FileDemo(object):
+	def index(self):
+		return """
+		<html><body>
+		<h2>Upload a file</h2>
+		<form action="upload" method="post" enctype="multipart/form-data">
+		filename: <input type="file" name="myFile" /><br />
+		<input type="submit" />
+		</form>
+		<h2>Download a file</h2>
+		<a href='download'>This one</a>
+		</body></html>
+		"""
+	index.exposed = True
+	
+	def view(self):
+		return """
+		<html>
+		<head><meta http-equiv="refresh" content="1"></head>
+		<body>
+		<img src="http://192.168.1.64:9000/temp.png" alt="Smiley face" height="1000" width="1000"/>
+		</body></html>
+		"""
+	view.exposed = True
+		
+	def upload(self, myFile):
+		out = """<html>
+		<body>
+		myFile length: %s<br />
+		myFile filename: %s<br />
+		myFile mime-type: %s
+		</body>
+		</html>"""
+		# Although this just counts the file length, it demonstrates
+		# how to read large files in chunks instead of all at once.
+		# CherryPy uses Python's cgi module to read the uploaded file
+		# into a temporary file; myFile.file.read reads from that.
+		size = 0
+		allData=''
+		while True:
+			data = myFile.file.read(8192)
+			allData+=data
+			if not data:
+				break
+			
+		size += len(allData)
+		savedFile=open(myFile.filename, 'wb')
+		savedFile.write(allData)
+		savedFile.close()
+		return out % (size, myFile.filename, myFile.type)
+		#~ return allData
+	upload.exposed = True
+
+cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+cherrypy.config.update({'server.socket_port': 8080,})
+cherrypy.quickstart(FileDemo())
+
